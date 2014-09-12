323e2bacdb58b37c95770608854b36a4bac20588
Jay Dave <jaycdave@gmail.com>
2014-09-11 22:44:39 -0700

    moved the file parser to model and added instance variable

---
+++ app/models/song_importer.rb
@@ -0,0 +1,45 @@
+
+
+$song_hash = {}
+
+class FileParser
+
+	attr_reader :username
+
+	def initialize#(username)
+		
+		@filename = '../../db/data/songs.txt'
+
+	end
+
+	def read_file
+
+		File.open(@filename, 'r') do |file|
+			index=1
+			while line = file.gets
+				if(line!="\n")
+					if index%2 != 0
+						artist = line.sub("\n","")
+					else
+						answer =line.sub("\n","")
+						$song_hash[artist] = answer
+					end
+					index += 1
+				end
+			end
+
+		end
+
+		# Test
+		 $song_hash #.each do |artist,path|
+		# 	p "#{artist}:#{path}"
+		# end
+
+	end
+
+end
+
+
+file_parser = FileParser.new()
+
+puts file_parser.read_file
